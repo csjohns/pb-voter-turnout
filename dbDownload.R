ea3b384f67c57eef61918df5875575bd9f302aff
Carolina Johnson <csjohns@uw.edu>
2017-04-28 16:12:39 -0700

    Correction to dbDownload function

--- dbDownload.R
+++ dbDownload.R
@@ -2,7 +2,7 @@ dbDownload <- function(table,...){
   
   con <- dbConnect(MySQL(),...) #establish connection to DB
   query <- paste("SELECT * FROM ", table, ";",sep = "")
-  rs <- dbSendQuery(con, "SELECT * FROM electiondistricts;") #defines the query; this SQL here is the very basic query to pull the whole table, you can send any SQL that you like
+  rs <- dbSendQuery(con, query) #defines the query; this SQL here is the very basic query to pull the whole table, you can send any SQL that you like
   db <- dbFetch(rs, n = -1) # -1 indicates to fetch all rows returned by query; can limit to only the first n rows (default appears to be 500)
   status_completed <- dbHasCompleted(rs)  # check whether query has completed with all results returned
   dbClearResult(rs)  #clears the resources used by the query - results saved to dataframe stay
--- ed_explore.R
+++ ed_explore.R
@@ -4,4 +4,6 @@ source("credentials.R") # loads the access credentials
 source("dbDownload.R")
 
 ed <- dbDownload(table = "electiondistricts", username = username, password = password, dbname = db.name, host = hostname, port = port)
+data <- dbDownload(table = "pb", username = username, password = password, dbname = db.name, host = hostname, port = port)
 rm(password, username, hostname, db.name, port) # if you want to remove the credentials from your environment 
+
