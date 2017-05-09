9ab63e1cd2045cc4de7ae4759891e89e03a8caea
Carolina Johnson <csjohns@uw.edu>
2017-05-08 22:34:29 -0700

    HTML map generation; new ED-PB comparison plots

--- ed_explore.R
+++ ed_explore.R
@@ -34,37 +34,57 @@ ed$ED[str_detect(ed$ED, "Tupper")]
 ## Calculating ED turnout among PB voters
 
 ## summarize - count by ED, sum votes by ED
-pb_turnout <- pb %>% group_by(ED) %>%
+pb_turnout <- pb %>% group_by(ED, County) %>%
   summarise(n = n(),
             G2015 = sum(`2015G` == "Y")/n,
             G2014 = sum(`2014G` == "Y")/n,
             G2013 = sum(`2013G` == "Y")/n,
             G2012 = sum(`2012G` == "Y")/n
             ) %>%
-  mutate(type = "pb")
-  
+  mutate(type = "pb",
+         County = tolower(County))
   
 
-ed_turnout <- ed %>% group_by(ED) %>%
+ed_turnout <- ed %>% group_by(ED, County) %>%
   summarise(n = sum(Reg),
             G2016 = sum(`2016G`)/n,
             G2015 = sum(`2015G`)/n,
             G2014 = sum(`2014G`)/n,
             G2013 = sum(`2013G`)/n
   ) %>%
-  mutate(type = "ed")
+  mutate(type = "ed",
+         County = tolower(County))
 
+# Joining PB voter turnout and ED turnout
 turnout <- full_join(pb_turnout, ed_turnout)
+# Checking full match:
 nrow(ed_turnout)+nrow(pb_turnout)==nrow(turnout)
 
+# Reshaping long for plotting
 turnout <- turnout %>% gather(year, turnout, starts_with("G")) %>%
   mutate(year = as.numeric(str_replace(year, "G", "")))
 
-ggplot(turnout, aes(x = year, y = turnout, color = type)) +
-  geom_jitter(alpha = .5)
+# Uninspiring plot
+pt <- ggplot(turnout, aes(x = as.factor(year), y = turnout, color = type))
+pt + geom_jitter(alpha = 0.5)+ labs(x = "Year", y = "Turnout") + theme_minimal() + facet_wrap(~type)
+ggsave("turnout_scatter.pdf")
+  
+pt + geom_boxplot(notch = TRUE) + labs(x = "Year", y = "Turnout") + theme_minimal()
+ggsave("turnout_boxplot.pdf", width = 8, units = "in")
 
+turnout %>%
+  filter(County %in% c("kings", "richmond", "queens", "new york", "bronx")) %>%
+  ggplot(aes(x = as.factor(year), y = turnout)) +
+  geom_jitter(aes(color = as.factor(County)),alpha = 0.5)+ labs(color = "County", x = "Year", y = "Turnout", title = "Turnout in NYC EDs") + 
+  theme_minimal() + facet_wrap(~type) +theme(legend.position = "bottom")
+ggsave("turnout_scatter_county.pdf", width = 8, units = "in")
 
-
+turnout %>%
+  filter(County %in% c("kings", "richmond", "queens", "new york", "bronx")) %>%
+  ggplot(aes(x = as.factor(year), y = turnout)) +
+  geom_boxplot(aes(color = as.factor(type)),alpha = 0.5)+ labs(color = "Turnout type", x = "Year", y = "Turnout", title = "Turnout in NYC EDs") + 
+  theme_minimal() + facet_wrap(~County) +theme(legend.position = "bottom")
+ggsave("turnout_boxplot_county.pdf", width = 10, height = 7, units = "in")
 
 
 
