6e6e3f0751f449bb32b744b9f04c7a86eb05df17
Carolina Johnson <csjohns@uw.edu>
2017-08-09 16:55:25 -0700

    Cleaned up preprocessing for individual-level regressions

--- pbvoters_regression.R
+++ pbvoters_regression.R
@@ -1,58 +1,113 @@
-## Exploring initial PB voter regression
+library(RColorBrewer)
+library(dplyr)
 library(tidyr)
+library(lubridate)
+library(stringr)
+library(margins)
+
+
+source("credentials.R") # loads the access credentials
+source("dbDownload.R")
+
+stringNAs <- function(x){
+  ifelse(x, "", NA)
+}
+
+### Loading and checking the data ###
+
+pb_orig <- dbDownload(table = "pb", username = username, password = password, dbname = db.name, host = hostname, port = port)
+rm(password, username, hostname, db.name, port) # if you want to remove the credentials from your environment 
+pb <- pb_orig
+
+
+
+## Exploring initial PB voter regression
 
 names(pb)
 
+## renaming/recoding variables----
 pb <- pb %>%
   rename(g_2012 = `2012G`,
          g_2013 = `2013G`,
          g_2014 = `2014G`,
          g_2015 = `2015G`,
-         p_2016 = `2016PP`,
-         p_2008 = `2008PP`,
+         pp_2016 = `2016PP`,
+         pp_2008 = `2008PP`,
          p_2012 = `2012P`,
          p_2013 = `2013P`,
          p_2014 = `2014P`,
          p_2015 = `2015P`,
+         p_2016 = `2016P`,
          g_2016 = `2016G`,
          g_2010 = `2010G`,
          g_2008 = `2008G`,
          g_2009 = `2009G`,
          g_2011 = `2011G`
-  )
+  ) 
 
-pb_long <- pb %>% select(-starts_with("g_2"), -starts_with("p_2")) %>%
+pb <- pb %>% rename(pb_2012 = `2012PB`,
+                    pb_2013 = `2013PB`,
+                    pb_2014 = `2014PB`,
+                    pb_2015 = `2015PB`,
+                    pb_2016 = `2016PB`) %>%
+  mutate_at(vars(starts_with("pb_")), funs(ifelse(. == "", 0, 1)))
+
+
+
+pb <- pb %>% 
+  group_by(VANID) %>%
+  mutate(DoB = ifelse(DoB == "", NA, DoB),
+         DoB = as.Date(format(as.Date(DoB, format = "%m/%d/%y"), "19%y-%m-%d")),
+         totpb = sum(pb_2012, pb_2013, pb_2014, pb_2015 , pb_2016, na.rm = T)
+)
+
+
+## Reshaping long for regression ----------------------------------------------------------------
+pb_long <- pb %>% filter(totpb >0) %>% select(VANID, totpb, starts_with("pb_")) %>%
   gather( year, pb, starts_with("pb")) %>%
   mutate(year = as.numeric(str_replace(year, "pb_", "")),
          pb = as.numeric(pb))
 glimpse(pb_long)
+summary(pb_long)
 
 pb_long <- pb_long %>% group_by(VANID) %>%
   arrange(VANID, year) %>%
   mutate(pbyear  = ifelse(pb == 1, year, NA),
-         pb_start = min(pbyear, na.rm = TRUE),
-         pb_count = sum(pb, na.rm = TRUE))
+         pb_start = min(pbyear, na.rm = TRUE)
+         )
 summary(pb_long)
 
 elec_long <- pb %>% select(-starts_with("pb_")) %>%
   gather(election, turned_out, starts_with("p_2"), starts_with("g_2")) %>%
   separate(election, c("election_type", "year")) %>%
-  mutate(turned_out = ifelse(turned_out == "Y", 1, 0),
+  mutate(turned_out = ifelse(turned_out != '' & !is.na(turned_out), 1, 0),
          year = as.numeric(year)) 
 
 pb_long <- pb_long %>% full_join(elec_long)
 pb_long <- pb_long %>%
-  mutate(after_pb = year > pb_start,
-         #after_pb = ifelse(is.na(after_pb), 0, after_pb),
-         repeater = pb_count > 1)
+  mutate(pb = ifelse(is.na(pb), 0, pb),
+         after_pb = as.numeric(year > pb_start),
+         after_pb = ifelse(is.na(after_pb), 0, after_pb),
+         repeater = totpb > 1,
+         age_at_vote = year - year(DoB) )
 
+## preliminary year fixed effect regressions --------------------------------
+#define regression equation
 base_formula = turned_out ~ after_pb + as.factor(year)
+
 base_lm <- lm(base_formula, data = pb_long)
 summary(base_lm)
 
 base_logit <- glm(base_formula, data = pb_long, family = binomial())
 summary(base_logit)
 
-predict(base_logit, newdata = expand.grid(year = 2013, after_pb = c(FALSE,TRUE)), type = "response")
+predict(base_logit, newdata = expand.grid(year = 2013, after_pb = c(0,1)), type = "response")
 
-dydx(pb_long, base_logit, "after_pbTRUE", change = c(0,1))
+dydx(pb_long, base_logit, "after_pb", change = c(0,1))
+
+## FE logit with covariates ----------------------------------------
+covar_formula <- turned_out ~ after_pb + as.factor(year) + Race + Sex + age_at_vote
+covar_logit <- glm(covar_formula, data = pb_long, family = binomial())
+summary(covar_logit)
+
+dydx(pb_long, covar_logit, "after_pbTRUE", change = c(0,1))
