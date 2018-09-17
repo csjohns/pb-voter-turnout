d3133cd08a1e610d1f40805a14bc126ab0150f3e
hjacobcarlson <h.jacob.carlson@gmail.com>
2018-09-17 14:40:25 -0400

    new descriptives charts

--- descriptives.R
+++ descriptives.R
@@ -2,6 +2,46 @@ library(tidyverse)
 library(data.table)
 library(lubridate)
 library(gridExtra)
+library(grid)
+
+source("credentials.R") # loads the access credentials
+source("dbDownload.R")
+
+stringNAs <- function(x){
+  ifelse(x, "", NA)
+}
+# 
+# conv19c <- function(s, ft = "%m/%d/%y"){
+#   as.Date(format(as.Date(s,format=ft), "19%y%m%d"), "%Y%m%d")
+# }
+### Load PB data ### -------------------------------------------------------------------------------------
+
+pb <- dbDownload(table = "pb", username = username, password = password, dbname = db.name, host = hostname, port = port)
+rm(password, username, hostname, db.name, port) # if you want to remove the credentials from your environment 
+# load("pb_orig.Rdata")
+#pb <- pb_orig 
+#rm(pb_orig)
+pb <- pb %>% select(-DWID) %>% 
+  filter(DoR != "" & !is.na(DoR)) %>% 
+  mutate_at(vars(starts_with("pb_2")), replace_na, 0) # this line is to deal with the fact that the row appended dist 23 voters (who didn't otherwise exist)
+
+pb <- pb %>% 
+  mutate(DoB = mdy(DoB),
+         pb = 1,
+         countycode = recode(County, BRONX = "005", KINGS = "047", `NEW YORK` = "061", QUEENS = "081", RICHMOND = "085")) %>%
+  mutate(countycode = ifelse(countycode %in% c("005", "047", "061", "081", "085"), countycode, NA),
+         tract = paste0(countycode, str_pad(CensusTract, 6, "left", "0")))
+         
+# limit to only 23/39 and 2016 districts
+pbnyc <- read.csv(file = "pbnyc_district_votes.csv", as.is = TRUE)
+pb2016 <- pbnyc %>% filter(districtCycle == 1 & voteYear == 2016) 
+
+### Load full voterfile data ### -------------------------------------------------------------------------------------
+### Limit it to only non-PB districts and VANIDS
+
+pbdistricts <- unique(pbnyc$district)
+rm(pbnyc)
+
 
 voterfile <- fread("PersonFile20180426-11056504994.txt")
 
@@ -52,8 +92,8 @@ voterfile <- voterfile %>%
   mutate(age = year(Sys.Date()) - year(DoB))
 
 # Merge in census tract data
-source("censustables.R")
-
+#source("censustables.R")
+load("census.Rdata")
 census <- race %>% 
   left_join(inc, by = "tract") %>% 
   left_join(educ, by = "tract")
@@ -66,6 +106,18 @@ voterfile <- voterfile %>%
   left_join(census, by = "tract") %>% 
   as.data.table()
 
+convLogicVote <- function(x){as.numeric(x != "")}   #function to create binary for any case with any non-empty string
+
+pb <- pb %>% mutate(pb = ifelse(is.na(pb), 0, pb))
+pb <- pb %>% 
+  mutate_at(vars(starts_with("p_")), funs(convLogicVote)) %>% 
+  mutate_at(vars(starts_with("g_")), funs(convLogicVote)) %>% 
+  mutate_at(vars(starts_with("pp_")), funs(convLogicVote)) %>% 
+  mutate(age =  year(Sys.Date()) - year(DoB))
+
+
+pb <- pb %>% 
+  left_join(census, by = "tract") 
 # Total registered voters == 4,946,176
 
 #### Turnout Registered Voters ####
@@ -115,10 +167,10 @@ turnout <- voterfile %>%
         Election = str_extract(elect, "[:alpha:]")) %>% 
   select(-elect, -tot)
 
-### all NYCDDs
+#### PB Districts
 
 turnout <- voterfile %>% 
-  group_by(NYCCD) %>%  
+  filter(NYCCD %in% pbdistricts) %>% 
   summarize(tot = n(),
             g_2017 = sum(g_2017, na.rm = TRUE)/tot,
             g_2016 = sum(g_2016, na.rm = TRUE)/tot,
@@ -156,13 +208,61 @@ turnout <- voterfile %>%
             p_2001 = sum(p_2001, na.rm = TRUE)/tot,
             p_2000 = sum(p_2000, na.rm = TRUE)/tot
   ) %>% 
-  mutate(Area = as.character(NYCCD)) %>%
-  filter(!is.na(NYCCD)) %>% 
-  gather(elect, Turnout, 3:37) %>% 
+  mutate(Area = "PB Districts") %>% 
+  gather(elect, Turnout, 2:36) %>% 
   mutate(Year = as.Date(str_extract(elect, "\\d{4}"), "%Y"),
          Election = str_extract(elect, "[:alpha:]")) %>% 
-  select(-elect, -tot, -NYCCD) %>% 
+  select(-elect, -tot) %>% 
   bind_rows(turnout)
+
+### PB Voters
+
+
+turnout <- pb %>% 
+  summarize(tot = n(),
+            g_2017 = sum(g_2017, na.rm = TRUE)/tot,
+            g_2016 = sum(g_2016, na.rm = TRUE)/tot,
+            g_2015 = sum(g_2015, na.rm = TRUE)/tot,
+            g_2014 = sum(g_2014, na.rm = TRUE)/tot,
+            g_2013 = sum(g_2013, na.rm = TRUE)/tot,
+            g_2012 = sum(g_2012, na.rm = TRUE)/tot,
+            g_2011 = sum(g_2011, na.rm = TRUE)/tot,
+            g_2010 = sum(g_2010, na.rm = TRUE)/tot,
+            g_2009 = sum(g_2009, na.rm = TRUE)/tot,
+            g_2008 = sum(g_2008, na.rm = TRUE)/tot,
+            g_2007 = sum(g_2007, na.rm = TRUE)/tot,
+            g_2006 = sum(g_2006, na.rm = TRUE)/tot,
+            g_2005 = sum(g_2005, na.rm = TRUE)/tot,
+            g_2004 = sum(g_2004, na.rm = TRUE)/tot,
+            g_2003 = sum(g_2003, na.rm = TRUE)/tot,
+            g_2002 = sum(g_2002, na.rm = TRUE)/tot,
+            g_2001 = sum(g_2001, na.rm = TRUE)/tot,
+            g_2000 = sum(g_2000, na.rm = TRUE)/tot,
+            p_2016 = sum(p_2016, na.rm = TRUE)/tot,
+            p_2015 = sum(p_2015, na.rm = TRUE)/tot,
+            p_2014 = sum(p_2014, na.rm = TRUE)/tot,
+            p_2013 = sum(p_2013, na.rm = TRUE)/tot,
+            p_2012 = sum(p_2012, na.rm = TRUE)/tot,
+            p_2011 = sum(p_2011, na.rm = TRUE)/tot,
+            p_2010 = sum(p_2010, na.rm = TRUE)/tot,
+            p_2009 = sum(p_2009, na.rm = TRUE)/tot,
+            p_2008 = sum(p_2008, na.rm = TRUE)/tot,
+            p_2007 = sum(p_2007, na.rm = TRUE)/tot,
+            p_2006 = sum(p_2006, na.rm = TRUE)/tot,
+            p_2005 = sum(p_2005, na.rm = TRUE)/tot,
+            p_2004 = sum(p_2004, na.rm = TRUE)/tot,
+            p_2003 = sum(p_2003, na.rm = TRUE)/tot,
+            p_2002 = sum(p_2002, na.rm = TRUE)/tot,
+            p_2001 = sum(p_2001, na.rm = TRUE)/tot,
+            p_2000 = sum(p_2000, na.rm = TRUE)/tot
+  ) %>% 
+  mutate(Area = "PB Voters") %>% 
+  gather(elect, Turnout, 2:36) %>% 
+  mutate(Year = as.Date(str_extract(elect, "\\d{4}"), "%Y"),
+         Election = str_extract(elect, "[:alpha:]")) %>% 
+  select(-elect, -tot) %>% 
+  bind_rows(turnout)
+
 
 ###### 
 
@@ -176,88 +276,280 @@ turnout <- turnout %>%
                          ifelse(year(Year) %in% midterm, "Midterm", 
                                 ifelse(year(Year) %in% citycouncil, "City Council", "Off Year"))))
 
-p_turnout <- ggplot(subset(turnout, Area %in% pbsampledists & Office != "Off Year"), 
+p_turnout <- ggplot(subset(turnout, Office != "Off Year"), 
             aes(x = Year, y = Turnout, group = interaction(Election, Office))) +
   geom_line(aes(linetype = factor(Election, labels = c("General", "Primary")))) +
   labs(linetype = "Election",
        x = "Office",
-       y = "District") +
+       y = "Turnout") +
   facet_grid(Area ~ Office) +
   theme_minimal() +
   theme(strip.placement = "outside",
         legend.margin = margin(r = 0.1, l = 0.01),
         axis.title.y.right = element_text(size = 9),
-        plot.margin = margin(r = 0.1, l = 0.1, t = 0.1, b = 0.1)) +
+        plot.margin = margin(r = 0.4, l = 0.4, t = 0.1, b = 0.1)) +
   scale_x_date(date_labels = "%y") +
   scale_y_continuous(position = "right")
+
+pdf(file = "turnout.pdf", height = 4, width = 4)
+p_turnout
+dev.off()
 
 
 #### Demographics #####
 
 ## Age
-
-p_age <- ggplot(subset(voterfile, NYCCD %in% pbsampledists), 
-                aes(x = age, group = NYCCD)) +
+library(scales)
+p1 <- ggplot(voterfile, 
+                aes(x = age)) +
   geom_histogram(bins = 15) +
-  facet_grid(NYCCD ~ .) +
-  labs(x = "Age",
-       y = "Individuals",
+  labs(x = "",
+       y = "",
        title = " ") +
-  ylim(0, 35000) +
+  coord_cartesian(xlim = c(0, 110),
+                  ylim = c(0, 2000000)) +
+  scale_y_continuous(labels = comma) +
+  scale_x_continuous(breaks = c(0, 15, 30, 45, 60, 75, 90, 105)) +
+  theme_minimal() +
+  theme(strip.text.y = element_blank(),
+        strip.text.x = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+
+p2 <- voterfile %>% 
+  filter(NYCCD %in% pbdistricts) %>% 
+  ggplot(aes(x = age)) +
+  geom_histogram(bins = 15) +
+  labs(x = "",
+       y = "",
+       title = " ") +
+  coord_cartesian(xlim = c(0, 110),
+                  ylim = c(0, 1200000)) +
+  scale_y_continuous(labels = comma) +
+  scale_x_continuous(breaks = c(0, 15, 30, 45, 60, 75, 90, 105)) +
+  theme_minimal() +
+  theme(strip.text.y = element_blank(),
+        strip.text.x = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+
+p3 <- ggplot(pb, aes(x = age)) +
+  geom_histogram(bins = 15) +
+  labs(x = "Age",
+       y = "",
+       title = " ") +
+  coord_cartesian(xlim = c(0, 110),
+                  ylim = c(0, 8000)) +
+  scale_y_continuous(labels = comma) +
+  scale_x_continuous(breaks = c(0, 15, 30, 45, 60, 75, 90, 105)) +
   theme_minimal() +
   theme(strip.text.y = element_blank())
 
+
+pdf(file = "age.pdf", height = 4, width = 4)
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), ggplotGrob(p3), size = "last"))
+dev.off()
+
+
 ## College degree
 ##### still working here ####
-p_college <- ggplot(subset(voterfile, NYCCD %in% pbsampledists), 
-                    aes(x = college/100, group = interaction(NYCCD, Sex))) +
-  geom_histogram(aes(fill = Sex), bins = 15) +
-  facet_grid(NYCCD ~ .) +
+p1 <- ggplot(voterfile, 
+                    aes(x = college/100)) +
+  geom_histogram(bins = 15) +
   labs(x = "College degree, tract (%)",
        y = "",
        title = " ") +
-  ylim(0, 35000) +
+  coord_cartesian(xlim = c(0, 1),
+                  ylim = c(0, 2000000)) +
+  scale_y_continuous(labels = comma) +
   theme_minimal() +
   theme(strip.text.y = element_blank(),
-        axis.text.y = element_blank())
+        strip.text.x = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
 
-p_college
-## Household Income
-
-p_hhinc <- ggplot(subset(voterfile, NYCCD %in% pbsampledists), 
-                              aes(x = medhhinc, group = NYCCD)) +
+p2 <- voterfile %>% 
+  filter(NYCCD %in% pbdistricts) %>% 
+  ggplot(aes(x = college/100)) +
   geom_histogram(bins = 15) +
-  facet_grid(NYCCD ~ .) +
-  labs(x = "Med HH Inc, tract ($)",
+  labs(x = "College degree, tract (%)",
        y = "",
        title = " ") +
-  ylim(0, 35000) +
-  theme_minimal() + 
+  coord_cartesian(xlim = c(0,1),
+                  ylim = c(0,1200000)) +
+  scale_y_continuous(labels = comma) +
+  theme_minimal() +
   theme(strip.text.y = element_blank(),
-        axis.text.y = element_blank(),
-        plot.margin = margin(r = 0.1, l = 0.1, t = 0.1, b = 0.1))
+        strip.text.x = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+p3 <- ggplot(pb, aes(x = college/100)) +
+  geom_histogram(bins = 15) +
+  labs(x = "College degree, tract (%)",
+       y = "",
+       title = " ") +
+  coord_cartesian(xlim = c(0,1),
+                  ylim = c(0,8000)) +
+  scale_y_continuous(labels = comma) +
+  #scale_x_continuous(breaks = c(0, 15, 30, 45, 60, 75, 90, 105)) +
+  theme_minimal() +
+  theme(strip.text.y = element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+pdf(file = "college.pdf", height = 4, width = 4)
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), ggplotGrob(p3), size = "last"))
+dev.off()
+
+## Household Income
+
+p1 <- ggplot(voterfile, aes(x = medhhinc)) +
+  geom_histogram(bins = 15) +
+  labs(x = "",
+       y = "",
+       title = " ") +
+  coord_cartesian(xlim = c(0, 250000),
+                  ylim = c(0,2000000)) +
+  scale_y_continuous(labels = comma) +
+  theme_minimal() +
+  theme(strip.text.y = element_blank(),
+        strip.text.x = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+p2 <- voterfile %>% 
+  filter(NYCCD %in% pbdistricts) %>% 
+  ggplot(aes(x = medhhinc)) +
+  geom_histogram(bins = 15) +
+  labs(x = "",
+       y = "",
+       title = " ") +
+  coord_cartesian(xlim = c(0,250000),
+                  ylim = c(0,1200000)) +
+  scale_y_continuous(labels = comma) +
+  theme_minimal() +
+  theme(strip.text.y = element_blank(),
+        strip.text.x = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+p3 <- ggplot(pb, aes(x = medhhinc)) +
+  geom_histogram(bins = 15) +
+  labs(x = "Median Household Income ($)",
+       y = "",
+       title = " ") +
+  coord_cartesian(xlim = c(0, 250000),
+                  ylim = c(0,8000)) +
+  scale_y_continuous(labels = comma) +
+  #scale_x_continuous(breaks = c(0, 15, 30, 45, 60, 75, 90, 105)) +
+  theme_minimal() +
+  theme(strip.text.y = element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+pdf(file = "hhinc.pdf", height = 4, width = 4)
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), ggplotGrob(p3), size = "last"))
+dev.off()
 
 ## Race
 
-p_race <- voterfile %>% 
-  filter(NYCCD %in% pbsampledists) %>% 
+
+p1 <- voterfile %>% 
   filter(Race != "N") %>% 
-  ggplot(aes(x = Race, group = NYCCD)) +
+  ggplot(aes(x = Race)) +
   geom_bar() +
-  facet_grid(NYCCD ~ .) +
-  labs(x = "Estimated Race",
+  labs(x = "",
        y = "",
        title = " ") +
-  ylim(0, 35000) +
+  coord_cartesian(ylim = c(0,2000000)) +
+  scale_y_continuous(labels = comma) +
   scale_x_discrete(labels = c("A" = "Asian", "B" = "Black", "H" = "Hispanic", "U" = "Unknown", "W" = "White")) +
   theme_minimal() +
   theme(strip.text.y = element_blank(),
-        axis.text.y = element_blank())
+        strip.text.x = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+p2 <- voterfile %>% 
+  filter(NYCCD %in% pbdistricts) %>% 
+  filter(Race != "N") %>% 
+  ggplot(aes(x = Race)) +
+  geom_bar() +
+  labs(x = "Estimated Race",
+       y = "",
+       title = " ") +
+  coord_cartesian(ylim = c(0,1200000)) +
+  scale_y_continuous(labels = comma) +
+  scale_x_discrete(labels = c("A" = "Asian", "B" = "Black", "H" = "Hispanic", "U" = "Unknown", "W" = "White")) +
+  theme_minimal() +
+  theme(strip.text.y = element_blank(),
+        strip.text.x = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+p3 <- pb %>% 
+  filter(Race != "N") %>% 
+  ggplot(aes(x = Race)) +
+  geom_bar() +
+  labs(x = "Estimated Race",
+       y = "",
+       title = " ") +
+  coord_cartesian(ylim = c(0,8000)) +
+  scale_y_continuous(labels = comma) +
+  scale_x_discrete(labels = c("A" = "Asian", "B" = "Black", "H" = "Hispanic", "U" = "Unknown", "W" = "White")) +
+  theme_minimal() +
+  theme(strip.text.y = element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+pdf(file = "race.pdf", height = 4, width = 4)
+
+grid.newpage()
+
+grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), ggplotGrob(p3), size = "last"))
+dev.off()
 
 
-library(ggpubr)
-
-p <- ggarrange(plotlist = list(p_age, p_college, p_race, p_hhinc, p_turnout), 
-               nrow = 1, ncol = 5,
-               widths = c(.8, .8, .8, .8, 1.4))
-ggsave(file = "descriptives.png", p, width = 12, height = 8)
+# library(ggpubr)
+# 
+# 
+# p <- ggarrange(plotlist = list(p_age, p_college, p_race, p_hhinc, p_turnout), 
+#                nrow = 1, ncol = 5,
+#                widths = c(.8, .8, .8, .8, 1.4))
+# ggsave(file = "descriptives.png", p, width = 12, height = 8)
