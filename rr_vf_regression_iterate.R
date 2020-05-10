1c3e6c4fdb5fb9a447d0caec88bd1b16e28ccbd4
Carolina Johnson <csjohns@uw.edu>
2020-05-09 20:44:03 -0700

    parallel regressions - match spec resolved

--- rr_vf_regression_iterate.R
+++ rr_vf_regression_iterate.R
@@ -18,6 +18,7 @@
 library(dplyr)
 library(tidyr)
 library(purrr)
+library(furrr)
 library(broom)
 library(ggplot2)
 library(lubridate)
@@ -69,7 +70,7 @@ preprocess_lmer <- function(match_res, model_form) {
 fit_lmer_model <- function(df, model_form) {
   ### run model
   res <- glmer(model_form, data = df, family = binomial(), nAGQ = 0) 
-  progbar$tick()$print()
+  # progbar$tick()$print()
   res
 }
 calc_margin_effect <- function(data, model_res){
@@ -97,7 +98,7 @@ voterfile <- readRDS(paste0("data/cleaned_R_results/voterfile_for_matching", suf
 # remove previously attached partial competitiveness records
 voterfile <- voterfile %>% 
   filter(VANID %in% uniquevanids) %>% 
-  select(-starts_with("comp_"), -pp_2016)
+  select(-starts_with("comp_"))
 
 
 ### Load competitiveness ---------------------------------------------------------------------------------
@@ -117,6 +118,7 @@ names(vf_compet) <- str_replace(names(vf_compet), "primary", "p")
   
 vf_compet <- vf_compet %>% 
   gather(key = election, value = compet, -VANID) %>% 
+  mutate(election = str_remove_all(election, "comp_")) %>%
   separate(election, c("year", "election_type")) %>% 
   mutate(year = as.numeric(year)) %>% 
   group_by(year, election_type) %>% 
@@ -133,19 +135,20 @@ allout <- allout %>%
 
 ### Define model formulas
 formula_df <- tibble(model_name = c("logit_minimal_form",
-                                    "logit_demog_form",
-                                    "logit_tract_form",
-                                    "lme_final_form",
+                                    # "logit_demog_form",
+                                    # "logit_tract_form",
+                                    # "lme_final_form",
                                     "lme_comp"),
                      model_formula = list(turned_out ~ pb + after_pb + as.factor(year) + election_type + (1| VANID) + (1|NYCCD),
-                                          turned_out ~ pb + after_pb  + as.factor(year) + election_type + Race + Female + age + I(age^2)  + (1| VANID) + (1|NYCCD),
-                                          turned_out ~ pb + after_pb  + as.factor(year) + election_type +  Race + Female + age + I(age^2)+ I(age_at_vote < 18) + college_pct + medhhinc_10k +(1| VANID) + (1|NYCCD),
+                                          # turned_out ~ pb + after_pb  + as.factor(year) + election_type + Race + Female + age + I(age^2)  + (1| VANID) + (1|NYCCD),
+                                          # turned_out ~ pb + after_pb  + as.factor(year) + election_type +  Race + Female + age + I(age^2)+ I(age_at_vote < 18) + college_pct + medhhinc_10k +(1| VANID) + (1|NYCCD),
                                           turned_out ~ pb + after_pb + Race + Female + as.factor(year) + election_type + age + I(age^2) + I(age_at_vote < 18) + medhhinc_10k + college_pct + majmatch + (1 | VANID) + (1|NYCCD),
                                           turned_out ~ pb + after_pb + Race + Female + as.factor(year) + election_type + compet + age + I(age^2) + I(age_at_vote < 18) + medhhinc_10k + college_pct + majmatch + (1 | VANID) + (1|NYCCD)
                      )) 
 
 allout <- expand_grid(allout, formula_df)
 ### Preprocess in list-columns --------------------------------------------------------------------------------------------
+plan(multiprocess)
 
 allout <- allout %>% 
   # slice(1:5) %>%
@@ -153,12 +156,13 @@ allout <- allout %>%
                                  model_form = model_formula),
                        .f = create_model_data) )
 
-progbar <- progress_estimated(nrow(allout))
+# progbar <- progress_estimated(nrow(allout))
 allout <- allout %>% 
-  # slice(18) %>%
-  mutate(result = pmap(.l = list(df = pblong,
+  # slice(1:2) %>%
+  mutate(result = future_pmap(.l = list(df = pblong,
                                  model_form = model_formula),
-                       .f = fit_lmer_model))
+                       .f = fit_lmer_model,
+                       .progress = TRUE))
 
 allout  %>% mutate(AIC = map_dbl(result, AIC), BIC = map_dbl(result, BIC)) %>% select(match_type, model_name, AIC, BIC) %>%  arrange(match_type, AIC) %>% View()
 glimpse(allout)
@@ -172,12 +176,13 @@ allout <- allout %>%
   mutate(tidyresult = map(result, broom::tidy, conf.int = TRUE),
          AIC = map_dbl(result, AIC),
          BIC = map_dbl(result, BIC), 
-         nvoter = map_dbl(pblong, ~n_distinct(.$VANID))) 
+         nvoter = map_dbl(pblong, ~n_distinct(.$VANID)),
+         pct39 = map_dbl(pblong, ~sum(.$pb == 1 & .$NYCCD == 39)/sum(.$pb == 1))) 
 
 allout <- mutate(allout, match_label = paste0(match_type, ": ", nvoter))
 
 lmers <- allout %>%  
-  select(match_type = match_label, model_name, tidyresult) %>% 
+  select(match_type = match_type, model_name, tidyresult, nvoter, pct39) %>% 
   unnest(cols = tidyresult) %>% 
   group_by(match_type, model_name) %>% 
   mutate(pb_effect = estimate[term == "after_pb"])
@@ -189,21 +194,23 @@ robust <- lmers %>%
     geom_point(position = position_dodge(width =.2 )) +
     geom_hline(aes(yintercept = 0)) +
     facet_wrap(~model_name) +
+    geom_text(aes(y = 2.5, label = nvoter), position = position_dodge(width = .2) , size = 2) +
+  geom_text(aes(y = -3, label = round(pct39,2)), position = position_dodge(width = .2) , size = 2) +
     # ylim(-5, 5) +
     coord_flip()
 robust
 plotly::ggplotly(robust)
 
 ## checking council district distribution across matches
-distabs <- allout %>% 
-  filter(match_type %in% c("All vars, fine", "All vars, coarse", "Dist medhhinc", "Excl comp + dist", "Excl compet", "Excl district")) %>% 
-  group_by(match_type) %>% slice(1) %>% ungroup %>% 
-  transmute(disttabs = map(pblong,~select(., VANID, NYCCD) %>% distinct() %>% count(NYCCD) %>% arrange(desc(n))),
-            nvoter = nvoter,
-            match_type = match_type) 
-full_join(distabs$disttabs[[1]] %>% rename(n_coarse = n),distabs$disttabs[[2]] %>% rename(n_fine = n)) %>% 
-  full_join(distabs$disttabs[[3]] %>% rename(n_medhhinc = n)) %>% 
-  full_join(distabs$disttabs[[4]] %>% rename(n_excl = n)) %>% 
-  full_join(distabs$disttabs[[5]] %>% rename(n_exclcomp = n)) %>% 
-  full_join(distabs$disttabs[[6]] %>% rename(n_excldist = n)) %>% 
-  mutate_at(vars(starts_with("n_")), ~./sum(., na.rm = T)) %>% View()
+# distabs <- allout %>% 
+#   filter(match_type %in% c("All vars, fine", "All vars, coarse", "Dist medhhinc", "Excl comp + dist", "Excl compet", "Excl district")) %>% 
+#   group_by(match_type) %>% slice(1) %>% ungroup %>% 
+#   transmute(disttabs = map(pblong,~select(., VANID, NYCCD) %>% distinct() %>% count(NYCCD) %>% arrange(desc(n))),
+#             nvoter = nvoter,
+#             match_type = match_type) 
+# full_join(distabs$disttabs[[1]] %>% rename(n_coarse = n),distabs$disttabs[[2]] %>% rename(n_fine = n)) %>% 
+#   full_join(distabs$disttabs[[3]] %>% rename(n_medhhinc = n)) %>% 
+#   full_join(distabs$disttabs[[4]] %>% rename(n_excl = n)) %>% 
+#   full_join(distabs$disttabs[[5]] %>% rename(n_exclcomp = n)) %>% 
+#   full_join(distabs$disttabs[[6]] %>% rename(n_excldist = n)) %>% 
+#   mutate_at(vars(starts_with("n_")), ~./sum(., na.rm = T)) %>% View()
